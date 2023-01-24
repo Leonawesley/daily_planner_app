80c2bc149b8b7c3b92919cda3ecc13a347f2195c
Leonawesley <leonajenishlin@gmail.com>
2023-01-24 21:00:52 +0000

    changes in js file

--- assests/js/index.js
+++ assests/js/index.js
@@ -30,7 +30,7 @@ $(function(){
             let timeStamp = `<div class="row">
                             <div class="col-2 hour">${loopTime}</div>
                             <textarea class="col-8 textArea present" id="${loopTime}" data-tense="${timeTense}"></textarea>
-                            <button class="col-2 saveBtn" id="${moment(i,"H").format("h")}" data-time="${loopTime}"><i class="fas fa-save"></i></button>
+                            <button class="col-2 saveBtn" data-time="${loopTime}"><i class="fas fa-save"></i></button>
                             </div>`;
             $(".container").append(timeStamp);
        }
@@ -38,7 +38,7 @@ $(function(){
             let timeStamp = `<div class="row">
                             <div class="col-2 hour">${loopTime}</div>
                             <textarea class="col-8 textArea past" id="${loopTime}" data-tense="${timeTense}" ></textarea>
-                            <button class="col-2 saveBtn" id="${moment(i,"H").format("h")}" data-time="${loopTime}"><i class="fas fa-save"></i></button>
+                            <button class="col-2 saveBtn" data-time="${loopTime}"><i class="fas fa-save"></i></button>
                             </div>`;
             $(".container").append(timeStamp);
        }
@@ -46,7 +46,7 @@ $(function(){
             let timeStamp = `<div class="row">
                             <div class="col-2 hour">${loopTime}</div>
                             <textarea class="col-8 textArea future" id="${loopTime}" data-tense="${timeTense}" ></textarea>
-                            <button class="col-2 saveBtn" id="${moment(i,"H").format("h")}" data-time="${loopTime}"><i class="fas fa-save"></i></button>
+                            <button class="col-2 saveBtn" data-time="${loopTime}"><i class="fas fa-save"></i></button>
                             </div>`;
             $(".container").append(timeStamp);
        }
@@ -54,85 +54,27 @@ $(function(){
 });
 
 
-// //To store the values in the textarea using local storage
-// $(document).on('click', '.saveBtn', function(event) {
-//     let eventItem = $(event.target).siblings("textarea").val();
-//     let eventTime = $(event.target).attr("data-time");
-//     console.log(eventItem);
-//     console.log(eventTime)
-//     localStorage.setItem(eventTime,eventItem);
+//To store the values in the textarea using local storage
+$(document).on('click', '.saveBtn', function(event) {
+    let eventItem = $(event.target).siblings("textarea").val();
+    let eventTime = $(event.target).attr("data-time");
+    console.log(eventItem);
+    console.log(eventTime)
+    localStorage.setItem(eventTime,eventItem);
+ });
 
-//  });
-
-//  //To retain the textarea details after refresh. Getting dettails from local storage
-//  $(window).on('load', function(){
-//     console.log("inside refresh")
-//     console.log(localStorage.getItem("9am"));
-//     console.log(localStorage.getItem("10am"));
-//     $("textarea").val("tets");
-//     $("#9am").val(localStorage.getItem("9am"));
-//     $("#10am").val(localStorage.getItem("10am"));
-//     $("#11am").val(localStorage.getItem("11am"));
-//     $("#12pm").val(localStorage.getItem("12pm"));
-//     $("#1pm").val(localStorage.getItem("1pm"));
-//     $("#2pm").val(localStorage.getItem("2pm"));
-//     $("#3pm").val(localStorage.getItem("3pm"));
-//     $("#4pm").val(localStorage.getItem("4pm"));
-//     $("#5pm").val(localStorage.getItem("5pm"));
-// });
-
-
-$(window).on('load', function() {
-    console.log((localStorage.getItem("9am")))
-    $("#9").on("click", function() {
-        localStorage.setItem("9am", $("#9am").val())
-    })
-    $("#9am").val(localStorage.getItem("9am"))
-  
-    console.log((localStorage.getItem("10am")))
-    $("#10").on("click", function() {
-        localStorage.setItem("10am", $("#10am").val())
-    })
-    $("#10am").text(localStorage.getItem("10am"))
-  
-    console.log((localStorage.getItem("11am")))
-    $("#11").on("click", function() {
-        localStorage.setItem("11am", $("#11am").val())
-    })
-    $("#11am").text(localStorage.getItem("11am"))
-  
-    $("#12").on("click", function() {
-        localStorage.setItem("12pm", $("#12pm").val())
-    })
-    $("#12pm").text(localStorage.getItem("12pm"))
-  
-    $("#1").on("click", function() {
-        localStorage.setItem("1pm", $("#1pm").val())
-    })
-    $("#1pm").text(localStorage.getItem("1pm"))
-  
-    $("#2").on("click", function() {
-        localStorage.setItem("2pm", $("#2pm").val())
-    })
-    $("#2pm").text(localStorage.getItem("2pm"))
-  
-    $("#3").on("click", function() {
-        localStorage.setItem("3pm", $("#3pm").val())
-    })
-    $("#3pm").text(localStorage.getItem("3pm"))
-  
-    $("#4").on("click", function() {
-        localStorage.setItem("4pm", $("#4pm").val())
-    })
-    $("#4pm").text(localStorage.getItem("4pm"))
-  
-    $("#5").on("click", function() {
-        localStorage.setItem("5pm", $("#5pm").val())
-    })
-    $("#5pm").text(localStorage.getItem("5pm"))
-    
-  })
-
+ //To retain the textarea details after refresh. Getting dettails from local storage
+ $(function(){
+    $("#9am").val(localStorage.getItem("9am"));
+    $("#10am").val(localStorage.getItem("10am"));
+    $("#11am").val(localStorage.getItem("11am"));
+    $("#12pm").val(localStorage.getItem("12pm"));
+    $("#1pm").val(localStorage.getItem("1pm"));
+    $("#2pm").val(localStorage.getItem("2pm"));
+    $("#3pm").val(localStorage.getItem("3pm"));
+    $("#4pm").val(localStorage.getItem("4pm"));
+    $("#5pm").val(localStorage.getItem("5pm"));
+});
 
 
 
